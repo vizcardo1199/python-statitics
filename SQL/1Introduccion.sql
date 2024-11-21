b60ec65702e19cefb958554958eec7df05067f94
vizcardo1199 <vizcardo1199@gmail.com>
2024-11-21 00:00:33 -0300

    FINAL SESION 7

---
+++ SQL/1Introduccion.sql
@@ -0,0 +1,11 @@
+-- como crear una tabla de la base de datos
+
+CREATE TABLE Usuarios(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(30) ,
+	correo VARCHAR(50)
+	);
+
+-- Como elimino una tabla de la base de datos
+
+DROP TABLE Usuarios;
