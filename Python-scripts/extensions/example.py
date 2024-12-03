d4740a32dbe5122515c7c22769fc4725f1907790
vizcardo1199 <vizcardo1199@gmail.com>
2024-12-02 21:08:09 -0500

    avance

---
+++ Python-scripts/extensions/example.py
@@ -0,0 +1,11 @@
+# Operadores Aritmeticos
+suma = 5+3 # operador suma
+resta = 5-10 #operador resta 
+producto = 5*10 # producto
+division = 5/10 # division
+#operador modulo es para obtener el residuo de la division
+print(20%3)
+
+#operador potencia '**'
+print(2**3)
+print(4**2)
---
+++ Python-scripts/extensions/introduccion.py
@@ -0,0 +1,11 @@
+print('hello world again')
+# comandos de consola 
+# l : para listar las carpetas 
+# cd : para dirigirnos a una carpeta en especifica ( aveces si la carpeta esta vacia no deja acceder)
+# clear : para limpiar la consola
+
+#operadores aritmeticos
+#solicitar un numero 
+m = int(input('ingresa un numero'))
+n = int(input('ingresa un numero'))
+print(m**n)
